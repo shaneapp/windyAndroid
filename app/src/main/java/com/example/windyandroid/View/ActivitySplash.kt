b2a492ea9bfe0003d6e074ce1a5f23e1f40ca28a
shane <shaneapp@hotmail.com>
2019-12-19 17:06:23 +0000

    Remove toolbar from theme

--- app/src/main/java/com/example/windyandroid/View/ActivitySplash.kt
+++ app/src/main/java/com/example/windyandroid/View/ActivitySplash.kt
@@ -1,16 +1,14 @@
 package com.example.windyandroid.View
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.windyandroid.R
 
 class ActivitySplash : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        super.onCreate(savedInstanceState, persistentState)
-
     }
 
 }
