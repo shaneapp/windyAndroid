bdddeedae114437ed04586d72c4a8b54343408be
shane <shaneapp@hotmail.com>
2019-12-19 16:54:35 +0000

    Add required gradle dependencies

---
+++ app/src/main/java/com/example/windyandroid/View/ActivitySplash.kt
@@ -0,0 +1,16 @@
+package com.example.windyandroid.View
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.windyandroid.R
+
+class ActivitySplash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        setContentView(R.layout.activity_splash)
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+
+}
