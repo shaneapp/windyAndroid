1ea2848ecd3fe5c18a3009ee5677af22cf6323ed
shane <shaneapp@hotmail.com>
2019-12-30 12:22:48 +0000

    Use custom attr for triangle background color

--- app/src/main/java/com/example/windyandroid/View/Custom/TriangleShapeView.kt
+++ app/src/main/java/com/example/windyandroid/View/Custom/TriangleShapeView.kt
@@ -7,14 +7,35 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
+import com.example.windyandroid.R
 
 class TriangleShapeView : View {
 
+    private var color = 0
+    private val p = Paint()
+
     constructor(context: Context) : super(context)
 
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setup(attrs)
+    }
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        setup(attrs)
+    }
+
+    fun setup(attrs: AttributeSet) {
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.TriangleShapeView, 0, 0)
+
+        try {
+            color = typedArray.getColor(R.styleable.TriangleShapeView_triangleBackgroundColor, 0)
+
+            p.color = color
+            p.setShadowLayer(20f, 0f, 0f, Color.BLACK)
+        } finally {
+            typedArray.recycle()
+        }
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -25,10 +46,6 @@ class TriangleShapeView : View {
         path.lineTo(width.toFloat(), height.toFloat())
         path.lineTo(0f, height.toFloat())
         path.close()
-
-        val p = Paint()
-        p.color = Color.WHITE
-        p.setShadowLayer(20f, 0f, 0f, Color.BLACK)
 
         canvas.drawPath(path, p)
     }
